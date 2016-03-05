db9755deca0b3a24e729ae2bc6bec18a773e4a56
Mike <mikeadkison@gmail.com>
2016-03-05 03:20:31 -0500

    initial commit

---
+++ desktop/src/com/mygdx/game/cutscene_viewer/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mygdx.game.cutscene_viewer.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygdx.game.cutscene_viewer.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}
