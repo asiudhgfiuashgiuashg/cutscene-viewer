b7ba53023ee7fc13366b4f6c3c1ba3b4693dac2b
Mike <mikeadkison@gmail.com>
2016-03-13 17:02:34 -0400

    rename main loop class; add SpriteSheetMetadatParser class

---
+++ core/src/com/mygdx/game/cutscene_viewer/SpriteSheetMetadataParser.java
@@ -0,0 +1,12 @@
+package com.mygdx.game.cutscene_viewer;
+
+/**
+ * 
+ * @author elimonent
+ *
+ * a class for reading in spritesheet metadata and creating
+ * appropriate animations for a spritesheet based on that metadata
+ */
+public class SpriteSheetMetadataParser {
+
+}
